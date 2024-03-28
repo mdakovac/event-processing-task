193ef6bb4810f76d8e7809e277a46406efb0e41f
Matej Đakovac <matejd196@gmail.com>
2024-03-28 23:09:40 +0100

    feat: Use pubsub to publish events in generator

--- internal/cmd/generator/main.go
+++ internal/cmd/generator/main.go
@@ -1,22 +1,35 @@
 package main
 
 import (
+	"bytes"
+	"encoding/gob"
 	"log"
 	"time"
 
 	"github.com/Bitstarz-eng/event-processing-challenge/internal/generator"
+	"github.com/Bitstarz-eng/event-processing-challenge/pubsub"
+	"github.com/Bitstarz-eng/event-processing-challenge/util/env_vars"
 	"golang.org/x/net/context"
 )
 
 func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
 	defer cancel()
+
+	env_vars.SetEnvVars()
+
+	_, topics := pubsub.Setup("internal.generator")
 
 	eventCh := generator.Generate(ctx)
 
 	for event := range eventCh {
 		log.Printf("%#v\n", event)
+
+		var inputBuffer bytes.Buffer
+		gob.NewEncoder(&inputBuffer).Encode(event)
+		topics["CasinoEvent.create"].Publish(ctx, &pubsub.PubsubMessageType{Data: inputBuffer.Bytes()})
 	}
 
+	topics["CasinoEvent.create"].Stop()
 	log.Println("finished")
 }
